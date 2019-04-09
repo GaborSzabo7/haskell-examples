f817e89c2687370e705ad01d47390a82e8b329ef
gaszabo7 <gaszabo7@gmail.com>
2019-04-09 23:37:28 +0200

    foldl, foldr

--- examples.hs
+++ examples.hs
@@ -209,3 +209,43 @@ flip2 f = g
 
 flip3 :: (a -> b -> c) -> b -> a -> c
 flip3 f y x = f x y
+
+-- Maps and filters
+quicksort2 :: (Ord a) => [a] -> [a]
+quicksort2 [] = []
+quicksort2 (x:xs) =
+    let smallerSorted = quicksort (filter (<= x) xs)
+        biggerSorted  = quicksort (filter (> x) xs)
+    in smallerSorted ++ [x] ++ biggerSorted
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [100000,99999..])
+    where p x = mod x 3892 == 0
+
+chain  :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+    | even n = n : chain (div n 2)
+    | odd n  = n : chain (n*3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+    where isLong xs = length xs > 15
+
+sum3 :: (Num a) => [a] -> a
+sum3 xs = foldl (\acc x -> acc + x ) 0 xs
+
+sum4 :: (Num a) => [a] -> a
+sum4 = foldl (+) 0
+
+elem4 :: (Eq a) => a -> [a] -> Bool
+elem4 y ys = foldl (\acc x -> if x == y then True else acc) False ys
+
+map2 :: (a -> b) -> [a] -> [b]
+map2 f xs = foldr (\x acc -> f x : acc) [] xs
+
+map3 :: (a -> b) -> [a] -> [b]
+map3 f xs = foldl (\acc x -> acc ++ [f x]) [] xs
+
+sum5 :: (Num a) => [a] -> a
+sum5 = foldl1 (+)
